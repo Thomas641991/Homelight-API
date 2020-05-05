f6095d22c33359b5bc7066b815a6dcda4c168fd4
Unknown <thomas_641991@hotmail.com>
2020-05-05 18:25:47 +0200

    bugfix

--- install.sh
+++ install.sh
@@ -36,9 +36,9 @@ sudo systemctl enable mosquitto.service
 echo "Installing API"
 cd Homelight-API/
 sudo npm install --save
-+x ./server.js
-+x ./install.sh
-+x ./update.sh
+chmod +x ./server.js
+chmod +x ./install.sh
+chmod +x ./update.sh
 
 # Copy homelight-api.service to boot at start
 echo "Copy Homelight-API.service"
