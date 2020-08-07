189e507dd1a3d14ae786bb6bc61a7503da183f20
Unknown <thomas_641991@hotmail.com>
2020-08-07 14:25:07 +0200

    Auto stash before merge of "master" and "origin/master"

--- controllers/system_controller.js
+++ controllers/system_controller.js
@@ -5,12 +5,19 @@ module.exports ={
 
 	// Used to update to new version of API.
 	updateSystem(req, res, next) {
-		updateShellScript.stdout.on('data', (data) => {
+		
+		exec('../update.sh', {shell: '/bin/bash'}, (err, stdout, stderr) => {
 			res.sendStatus(200);
-			console.log(data);
-		})
-		updateShellScript.stderr.on('data', (data) => {
-			console.error(data);
-		})
+			console.log('OUTPUT', stdout, stderr);
+		});
+		
+		// console.log(req);
+		// updateShellScript.stdout.on('data', (data) => {
+		// 	res.sendStatus(200);
+		// 	console.log(data);
+		// })
+		// updateShellScript.stderr.on('data', (data) => {
+		// 	console.error(data);
+		// })
 	}
 }
