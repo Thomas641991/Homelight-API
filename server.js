394bd7dd5f828ea26727a10cac8ac17675d0aab8
Unknown <thomas_641991@hotmail.com>
2020-08-13 22:48:14 +0200

    Added console log showing ip address

--- server.js
+++ server.js
@@ -1,7 +1,27 @@
 const app = require('./app');
 const config = require('./config/env/env');
+const os = require('os');
 
 app.listen(config.env.webPort, () => {
-    console.log('Running on port ' + config.env.webPort);
-    console.log('Date and time started: ' + new Date())
+    console.log('Date and time started: ' + new Date());
+    
+    
+    console.log('Running on: ' + getIp() + ':' + config.env.webPort);
 });
+
+function getIp() {
+    const results = {};
+    for(const name of Object.keys(os.networkInterfaces())) {
+        for(const net of os.networkInterfaces()[name]) {
+            if (net.family === 'IPv4' && !net.internal) {
+                if (!results[name]) {
+                    results[name] = [];
+                }
+                
+                results[name].push(net.address);
+            }
+        }
+    }
+    
+    return results.Ethernet;
+}
